c386173009e4c24fd49448f09ae98e043582c614
Karen Rose <karen.rose807@gmail.com>
2018-06-02 22:32:26 -0500

    Add new Users component and format files

---
+++ src/components/Users.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Users</h1>
+        <label for="email_address">Email Address</label>
+        <input type="text" name="email_address" id="email_address" /><br />
+        <label for="first_name">First Name</label>
+        <input type="text" name="first_name" id="first_name" /><br />
+        <label for="last_name">Last Name</label>
+        <input type="text" name="last_name" id="last_name" /><br />
+        <button v-on:click="counter += 1">Submit</button>
+    </div>
+</template>
+
+<script>
+export default { name: 'Users' }
+</script>
+
+<style scoped>
+
+</style>
