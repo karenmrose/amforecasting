0a3f0c78426cd74d752e06b8d37291192ef04391
Karen Rose <karen.rose807@gmail.com>
2018-06-03 00:46:46 -0500

    Add GET /jobs route

--- server/job/routes.js
+++ server/job/routes.js
@@ -3,4 +3,12 @@
 const express = require('express')
 const router = express.Router()
 
+const JobModel = require('./model')
+
+router.get('/api/jobs', async (req, res) => {
+	const jobs = await JobModel.find().lean()
+
+	res.json(jobs)
+})
+
 module.exports = router
